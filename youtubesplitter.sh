85023d6ed7872f74a2ac839092a2630a11e15baf
Hamilkar247 <adonnibal96@gmail.com>
2020-09-29 10:33:23 +0200

    W0-0 poprawka dla szablonu - wczesniej w outputowych time-ach mielismy spacje

--- youtubesplitter.sh
+++ youtubesplitter.sh
@@ -6,24 +6,27 @@ function parting_video()
   while IFS= read -r line
   do
     echo "witam"
-    IFS='-' read -ra tablica <<< "$line"
+    IFS='- ' read -ra tablica <<< "$line"
     echo "${tablica[0]}"
     starttime="${tablica[0]}"
     endtime="${tablica[1]}"
     nameoutput="${tablica[2]}"" $namefile"
     echo "${tablica[1]}"
     echo "${tablica[2]}"
-    ffmpeg -ss $starttime -i "$namefile" -to $endtime -c copy "$nameoutput"
+    #ffmpeg -ss $starttime -i "$namefile" -to $endtime -c copy "$nameoutput"
+    #mp3-tagger-dane
   done < "$rozdzialy_timestamp"
 }
 
-youtube-dl -i --verbose --extract-audio --audio-format  mp3 "$1"
+#youtube-dl -i --verbose --extract-audio --audio-format  mp3 "$1"
 echo "Skończyłem pobierać"
 namefile="$(find . -name "*.mp3" -exec basename {} .po \;)"
 echo "$namefile"
 parting_video
-rm $namefile
-mv *.mp3 $MP3_Folder
+#rm $namefile
+#mv *.mp3 $MP3_Folder
+
+
 #var="$(awk 'NR==1 {print; exit}' parts.txt)"
 ##ffmpeg -ss "00:00:00" -i "$namefile" -to "00:05:00" -c copy output.mp3
 #IFS='-' read -ra ADDR <<< "$var"
